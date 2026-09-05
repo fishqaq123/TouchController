c1d14fd9f9626ee117a1667409c5142a853beaaf
fishqaq123 <145851012+fishqaq123@users.noreply.github.com>
2026-09-05 19:28:59 +0800

    feat(ui): 添加 TechnetiumTheme（仿 TC TouchControllerTheme）

---
+++ technetium/common/src/main/kotlin/top/technetium/ui/theme/TechnetiumTheme.kt
@@ -0,0 +1,49 @@
+/*
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ * Copyright (C) fishqaq123
+ */
+
+package top.technetium.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.staticCompositionLocalOf
+import top.fifthlight.combine.core.node.LocalInputHandler
+import top.fifthlight.combine.core.paint.BackgroundTexture
+import top.fifthlight.combine.core.paint.Drawable
+import top.fifthlight.combine.theme.LocalTheme
+import top.fifthlight.combine.theme.Theme
+import top.fifthlight.combine.theme.blackstone.BlackstoneTextures
+import top.fifthlight.combine.theme.blackstone.BlackstoneTheme
+
+val LocalTechnetiumTheme = staticCompositionLocalOf { TechnetiumTheme() }
+
+data class TechnetiumTheme(
+    val background: BackgroundTexture = BlackstoneTextures.background,
+    val borderBackgroundDark: Drawable = BlackstoneTextures.widget_background_background_dark,
+
+    val appBarBackground: Drawable = BlackstoneTextures.widget_background_background_gray_title,
+
+    val titleBoxBackground: Drawable = BlackstoneTextures.widget_background_background_lightgray_title,
+
+    val base: Theme = BlackstoneTheme,
+) {
+    companion object {
+        val default = TechnetiumTheme()
+
+        @Composable
+        inline operator fun invoke(crossinline block: @Composable TechnetiumTheme.() -> Unit) {
+            default(block)
+        }
+    }
+}
+
+@Composable
+inline operator fun TechnetiumTheme.invoke(crossinline block: @Composable TechnetiumTheme.() -> Unit) {
+    CompositionLocalProvider(
+        LocalTechnetiumTheme provides TechnetiumTheme(),
+        LocalTheme provides base,
+    ) {
+        block()
+    }
+}
